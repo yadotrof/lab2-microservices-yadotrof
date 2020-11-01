19082a3d62eeb3351a6cfdaddab90cdb06971bcd
Gleb Shmarov <gshmarov@parallels.com>
2020-11-02 00:05:39 +0300

    Fix warranty health url.

--- warranty_service/warranty/urls.py
+++ warranty_service/warranty/urls.py
@@ -4,6 +4,5 @@ from . import views
 urlpatterns = [
     re_path(r'^(?P<item_uuid>[0-9a-fA-F-]+)$', views.warranty_actions),
     re_path(r'^(?P<item_uuid>[0-9a-fA-F-]+)/warranty$', views.request_warranty),
-    path('ht/', include('health_check.urls')),
 ]
 
--- warranty_service/warranty_service/urls.py
+++ warranty_service/warranty_service/urls.py
@@ -3,5 +3,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/warranty/', include('warranty.urls'))
+    path('api/v1/warranty/', include('warranty.urls')),
+    path('ht/', include('health_check.urls')),
 ]
